79a0ef92906b104af477594fc576f001026f0371
user <user@users-MacBook-Pro.local>
2021-07-22 13:30:45 +0200

    product route and controller, gets searched prods

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const expressValidator = require("express-validator");
 require("dotenv").config();
-//import routes
+
+// routes middleware
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
@@ -28,6 +30,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 //routes middleware
 app.use("/api", authRoutes);
