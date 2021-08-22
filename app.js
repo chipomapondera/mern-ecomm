f08c5af7c769d31fc9d044ed2fbc258bda614f41
chipomapondera <chipomapondera@gmail.com>
2021-08-22 16:55:11 +0200

    added braintree (payments) routes, controller

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const cors = require("cors");
 const expressValidator = require("express-validator");
 require("dotenv").config();
 
-// routes middleware
+// import routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const braintreeRoutes = require("./routes/braintree");
 
 //app
 const app = express();
@@ -37,6 +38,7 @@ app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", braintreeRoutes);
 
 const port = process.env.PORT || 8000;
 
